1bac21a19acaf88dd1ea006a0065e675719d10b5
unknown <you@example.com>
2023-11-05 00:58:44 +0600

    added how it work sections as a extra static section in home page

---
+++ src/Components/Howwork.jsx
@@ -0,0 +1,39 @@
+
+import post from '../assets/icons2/icon4-1.svg'
+import find from '../assets/icons2/icon5.svg'
+import pay from '../assets/icons2/icon8.svg'
+import halp from '../assets/icons2/icon7.svg'
+
+
+const HowWork = () => {
+    return (
+        <div className='bg-[#123841]'>
+            <h1 className="text-5xl pt-14 text-[#64666C] mb-3 font-bold text-center">How It Work
+            </h1>
+            <p className="text-center text-[#64666C]">Streamline your hiring process with strategic channels to reach qualified candidates
+            </p>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-8'>
+                <div className='flex flex-col items-center justify-center gap-y-3 text-[#64666C]'>
+                    <img className='w-14 rounded-full' src={halp} alt="" />
+                    <h3 className='text-2xl font-bold'>1.Post A Job</h3>
+                </div>
+                <div className='flex flex-col items-center justify-center gap-y-3 text-[#64666C]'>
+                    <img className='w-14 rounded-full' src={pay} alt="" />
+                    <h3 className='text-2xl font-bold'>2. Find Employers</h3>
+                </div>
+                <div className='flex flex-col items-center justify-center gap-y-3 text-[#64666C]'>
+                    <img className='w-14 rounded-full' src={post} alt="" />
+                    <h3 className='text-2xl font-bold'>3. Pay Safely</h3>
+                </div>
+                <div className='flex flex-col items-center justify-center gap-y-3 text-[#64666C]'>
+                    <img className='w-14 rounded-full' src={find} alt="" />
+                    <h3 className='text-2xl font-bold'>4. We’re Here To Help</h3>
+                </div>
+
+
+            </div>
+        </div>
+    );
+};
+
+export default HowWork;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,12 @@
+import HowWork from "../Components/Howwork";
 
 const Home = () => {
     return (
         <div>
 
+            <div>
+                <HowWork></HowWork>
+            </div>
         </div>
     );
 };
